c9d75ed77f84379a36ff7d98908c3abeb00c06a8
Victor Massé <victor.dhamma@gmail.com>
2021-07-19 07:12:08 +0200

    reorganize styles

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -3,31 +3,3 @@
   <li>task2</li>
   <li>task3</li>
 </ul>
-
-<style>
-  ul {
-    margin: 0;
-    padding: 0;
-  }
-
-  li {
-    position: relative;
-    list-style-type: none;
-    padding-left: 2rem;
-  }
-
-  li:not(:last-child) {
-    border-bottom: 1px solid #dee2e6;
-  }
-
-  li:hover {
-    color: #198754;
-  }
-
-  li:hover::before {
-    position: absolute;
-    top: 0;
-    left: 0;
-    content: url("/check.svg");
-  }
-</style>
