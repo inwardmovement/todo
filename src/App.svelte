c97fc2f7eab05ffed547b4a8a894832befbde963
Victor Massé <victor.dhamma@gmail.com>
2021-07-21 20:27:56 +0200

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,11 @@
 <script>
   import Todos from "./components/Todos.svelte";
+
+  let todos = [
+    { id: 1, name: 'task1'},
+    { id: 2, name: 'task2'},
+    { id: 3, name: 'task3'}
+  ]
 </script>
 
-<Todos />
+<Todos {todos} />
--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,5 +1,26 @@
+<script>
+  export let todos = []
+
+  $: totalTodos = todos.length
+  let newTodo = ''
+  $: newTodoId = totalTodos ? Math.max(...todos.map(t => t.id)) + 1 : 1
+
+  function removeTodo(todo) {
+      todos = todos.filter(t => t.id !== todo.id)
+    }
+
+  function addTodo() {
+    todos = [...todos, { id: newTodoId, name: newTodo }]
+    newTodo = ''
+  }
+</script>
+
 <ul>
-  <li>task1</li>
-  <li>task2</li>
-  <li>task3</li>
+  {#each todos as todo (todo.id)}
+    <li id="todo-{todo.id}" on:click={() => removeTodo(todo)}>{todo.name}</li>
+  {/each}
 </ul>
+
+<form on:submit|preventDefault={addTodo}>
+  <input bind:value={newTodo} type="text" id="todo-new" />
+</form>
