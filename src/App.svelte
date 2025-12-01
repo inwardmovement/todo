cbf19f2e230d7fb4004abfea16cad552de2ef784
Victor Massé <victor.dhamma@gmail.com>
2025-12-01 11:00:28 +0100

    switch project to pure JS

--- src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import GlobalStylesReboot from "./components/globalStyles/GlobalStylesReboot.svelte"
-  import GlobalStyles from "./components/globalStyles/GlobalStyles.svelte"
-  import Todos from "./components/Todos.svelte"
-</script>
-
-<GlobalStylesReboot/>
-<GlobalStyles/>
-<Todos/>
